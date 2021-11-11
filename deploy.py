b918e688cf8d9675dbf32fb995e2172a05718f38
arideout1 <adamrideout@outlook.com>
2021-11-10 21:07:30 -0800

    Finished code

--- deploy.py
+++ deploy.py
@@ -2,10 +2,13 @@ from solcx import compile_standard, install_solc
 import json
 from web3 import Web3
 import os
+from dotenv import load_dotenv
+import web3
+
+load_dotenv()
 
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
-    print(simple_storage_file)
 
 install_solc("0.8.0")
 compiled_sol = compile_standard(
@@ -32,10 +35,15 @@ bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-chain_id = 5777
-my_address = "0x6B6F38700484C1Fcbcd75efBA73e982C5753583a"
-private_key = os.environ["PRIVATE_KEY"]
+w3 = Web3(
+    Web3.HTTPProvider("https://rinkeby.infura.io/v3/dab9f99ed8cf47c49c5e7f82577a8b86")
+)
+chain_id = 4
+
+my_address = "0x85b1e6128E559390DC427D006FbD52Ab6eEa3ca6"
+private_key = os.getenv("PRIVATE_KEY")
+print(private_key)
+
 
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 
@@ -46,3 +54,30 @@ transaction = SimpleStorage.constructor().buildTransaction(
 )
 
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+
+print("Deploying contract..")
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Deployed!")
+
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+print(simple_storage.functions.retrieve().call())
+
+print("Updating Contract...")
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce + 1,
+    }
+)
+
+signed_store_tx = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+
+send_store_tx = w3.eth.send_raw_transaction(signed_store_tx.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated!")
+
+print(simple_storage.functions.retrieve().call())
